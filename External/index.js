b98614a77f1403b2cc257b6d6cbfb585d2cb0370
joy <ciftekharulislam@gmail.com>
2021-07-11 10:56:30 -0700

    external folder added

---
+++ External/index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const axios   = require('axios')
+
+const port = 8081;
+
+app.use(express.json())
+
+const app = express();
+
+const internalURL = "http://13.229.239.43:8081/api/v1/internal"
+
+app.get('/status', (req, res) => res.send({status: "I'm alive!"}));
+
+app.get('/', (request,response) => {
+    response.send('External service')
+})
+
+app.get('/api/v1/add',async (request,response)=>{
+
+    console.info('requesting ', internalURL)
+    console.warn('make sure the internal URL is correct. If incorrect, change the "internalURL" variable and restart the container')
+  
+    try{
+      // request to the internal service 
+      const internalServiceResponse = await axios.get(internalURL)
+      
+      const exrternalServiceResponse = {
+        msg: 'request success',
+        internal_service_response: internalServiceResponse.data
+      }
+  
+      // send the response to the public internet
+      response.send(exrternalServiceResponse)
+    } catch(err){
+      // handle the error and send response
+      const errResponse = {
+        msg: 'got error'
+      }
+      response.send(errResponse)
+      // console.log(err)
+    }
+  })
+
+
+app.listen(port, () => console.log(`External port listening ${port}!`));
