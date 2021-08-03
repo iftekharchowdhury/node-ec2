7caab799c80ae145200afd1d45f334c06351f424
joy <ciftekharulislam@gmail.com>
2021-08-03 18:25:03 +0600

    changed internal ip

--- External/index.js
+++ External/index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json())
 
 
-const internalURL = "http://172.17.0.2:80/api/v1/internal"
+const internalURL = "http://172.17.0.3:8082/api/v1/internal"
 
 
 app.get('/status', (req, res) => res.send({status: "I'm alive!"}));
